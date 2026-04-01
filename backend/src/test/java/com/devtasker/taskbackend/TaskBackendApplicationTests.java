180736f5275c0a86395bb4ff7fdd910f291512b5
Maciej Modzelewski <maciej.modzelewski@version1.com>
2026-04-01 10:49:20 +0100

    Frontend moved to a separate directory and backend initialised

---
+++ backend/src/test/java/com/devtasker/taskbackend/TaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtasker.taskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
